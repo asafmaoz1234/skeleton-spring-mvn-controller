bafff508f9facc421bf70568bea53a18c72d9107
asafmaoz1234 <asafmaoz27@gmail.com>
2022-08-08 20:21:17 +0300

    added postman collection, finalized controller and payload

--- src/main/java/com/example/springbootmvncontroller/SamplePayload.java
+++ src/main/java/com/example/springbootmvncontroller/SamplePayload.java
@@ -1,5 +1,17 @@
 package com.example.springbootmvncontroller;
 
-public class SamplePayload {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.stereotype.Service;
 
+@Service
+public class SamplePayload {
+    @JsonProperty("payload_data")
+    private String payloadData;
+
+    @Override
+    public String toString() {
+        return "SamplePayload{" +
+                "payloadData='" + payloadData + '\'' +
+                '}';
+    }
 }
